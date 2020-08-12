b316b8776e2448ba2cc419ad652d406f17485906
himkt <himkt@cookpad.com>
2020-08-13 00:16:32 +0900

    feat: Add option to specify maxID

--- main.go
+++ main.go
@@ -2,35 +2,34 @@ package main
 
 import (
   "flag"
-	"fmt"
-	"os"
+  "fmt"
+  "os"
 
-	"github.com/dghubble/go-twitter/twitter"
-	"github.com/dghubble/oauth1"
+  "github.com/dghubble/go-twitter/twitter"
+  "github.com/dghubble/oauth1"
 )
 
-
 func createClient() *twitter.Client {
-	APIToken            := os.Getenv("API_KEY")
-	APITokenSecret      := os.Getenv("API_KEY_SECRET")
-	consumerToken       := os.Getenv("CONSUMER_KEY")
-	consumerTokenSecret := os.Getenv("CONSUMER_KEY_SECRET")
+  APIToken := os.Getenv("API_KEY")
+  APITokenSecret := os.Getenv("API_KEY_SECRET")
+  consumerToken := os.Getenv("CONSUMER_KEY")
+  consumerTokenSecret := os.Getenv("CONSUMER_KEY_SECRET")
 
-	config              := oauth1.NewConfig(APIToken, APITokenSecret)
-	token               := oauth1.NewToken(consumerToken, consumerTokenSecret)
-	httpClient          := config.Client(oauth1.NoContext, token)
+  config := oauth1.NewConfig(APIToken, APITokenSecret)
+  token := oauth1.NewToken(consumerToken, consumerTokenSecret)
+  httpClient := config.Client(oauth1.NoContext, token)
 
-	return twitter.NewClient(httpClient)
+  return twitter.NewClient(httpClient)
 }
-
 
 func main() {
   var (
+    c = flag.Int("batch", 50, "Number of tweets to fetch")
     v = flag.Bool("delete", false, "if dry-run is specified, it runs on dry-run mode")
-    s = flag.Bool("show-text", false, "if --show-text is specified, it shows tweets' texts")
-    t = flag.Int ("threshold", 5, "Minimum favorite count not to be deleted")
-    m = flag.Int ("max-iter", 10, "Number of iterate next cursor")
-    c = flag.Int ("bucket-size", 50, "Number of tweets to fetch")
+    s = flag.Bool("text", false, "show messages of tweets")
+    t = flag.Int("threshold", 5, "Minimum favorite count not to be deleted")
+    m = flag.Int("iter", 10, "Number of iterate next cursor")
+    i = flag.Int64("maxId", 8888888888888888888, "Maximum number of tweet ID")
   )
   flag.Parse()
 
@@ -38,18 +37,18 @@ func main() {
   screenName := os.Getenv("SCREEN_NAME")
 
   // constant for MAX TWEET ID
-  var maxID int64 = 8888888888888888888
+  var maxID int64 = *i
   var messagePrefix string
 
-	for i := 0; i < *m; i++ {
+  for i := 0; i < *m; i++ {
 
-		userTimelineParams := &twitter.UserTimelineParams{
-			ScreenName: screenName,
-			Count:      *c,
-			MaxID:      maxID,
-		}
+    userTimelineParams := &twitter.UserTimelineParams{
+      ScreenName: screenName,
+      Count:      *c,
+      MaxID:      maxID,
+    }
 
-		tweets, _, e := client.Timelines.UserTimeline(userTimelineParams)
+    tweets, _, e := client.Timelines.UserTimeline(userTimelineParams)
     if e != nil {
       fmt.Println(e)
       fmt.Println("Error")
@@ -57,21 +56,21 @@ func main() {
     }
 
     fmt.Println("Current MaxID: ", maxID)
-		for index := range tweets {
-			tweet := tweets[index]
+    for index := range tweets {
+      tweet := tweets[index]
 
       if maxID > tweet.ID {
         maxID = tweet.ID
       }
 
-			if tweet.FavoriteCount >= *t {
-				continue
-			}
+      if tweet.FavoriteCount >= *t {
+        continue
+      }
 
       if *v {
         messagePrefix = "Delete: "
         statusDestroyParams := &twitter.StatusDestroyParams{}
-			  client.Statuses.Destroy(tweet.ID, statusDestroyParams)
+        client.Statuses.Destroy(tweet.ID, statusDestroyParams)
 
       } else {
         messagePrefix = "[dry-run] Delete: "
@@ -81,6 +80,6 @@ func main() {
       if *s {
         fmt.Println(tweet.Text)
       }
-		}
-	}
+    }
+  }
 }
