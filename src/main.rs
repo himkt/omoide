e1c8c2adb62ee67f152c538ece46582f641f274b
Makoto Hiramatsu <himkt@cookpad.com>
2021-03-13 23:51:56 +0900

    Add command-line parser

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
 use egg_mode::{KeyPair, Token, tweet::delete};
+use structopt::StructOpt;
+
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "omoide")]
+struct Opt {
+    #[structopt(short, long)]
+    delete: bool,
+
+    #[structopt(short = "f", long)]
+    favorite_count: i32,
+
+    #[structopt(short, long)]
+    screen_name: String,
+}
 
 
 fn auth(
@@ -23,6 +38,7 @@ fn load_env(key: &str) -> String {
 
 #[tokio::main]
 async fn main() {
+    let opt: Opt = Opt::from_args();
     let consumer_key = load_env("CONSUMER_KEY");
     let consumer_secret = load_env("CONSUMER_KEY_SECRET");
     let access_key = load_env("API_KEY");
@@ -37,7 +53,7 @@ async fn main() {
     );
 
     let token = auth(consumer_key, consumer_secret, access_key, access_secret);
-    let user_id = egg_mode::user::show("tkmih", &token).await.unwrap().response.id;
+    let user_id = egg_mode::user::show(opt.screen_name, &token).await.unwrap().response.id;
 
     let mut max_id: Option<u64> = None;
     let timeline = egg_mode::tweet::user_timeline(user_id, false, false, &token);
@@ -45,9 +61,14 @@ async fn main() {
     for _ in 0..10 {
         let ret = timeline.call(None, max_id).await.unwrap();
         for status in ret.response.iter() {
-            if status.favorite_count < 1 {
-                println!("{:?}", status.text);
-                delete(status.id, &token).await.unwrap();
+            if status.favorite_count < opt.favorite_count {
+                if !opt.delete {
+                    println!("[dru-run] {:?}", status.text);
+                }
+                else {
+                    delete(status.id, &token).await.unwrap();
+                    println!("[deleted] {:?}", status.text);
+                }
             }
 
             max_id = Some(status.id);
